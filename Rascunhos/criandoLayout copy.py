2ad30a2d2a2f716576f103a698d7284751ab79e7
Cristiano Gregory Monfrim Camandaroba <88840308+Camandaroba06@users.noreply.github.com>
2022-09-15 22:07:00 -0300

    Add files via upload

---
+++ Rascunhos/criandoLayout copy.py
@@ -0,0 +1,100 @@
+import PySimpleGUI as sg
+from stopwatch import Stopwatch
+# ----------------  Create Form  ----------------
+minut = 0
+segundos = 0
+millis = 0
+stopwatch = Stopwatch(2)
+stopwatch.reset()
+
+
+
+sg.theme('Dark Amber')
+frame_layout = [
+                [sg.T('')],
+                [sg.T('')],
+                [sg.Combo(['Equipe 1','Equipe 2','Equipe 3','Equipe 4' ])],
+                [sg.T('')],
+                [sg.T('')],
+                [sg.Button('Run', key='-RUN-PAUSE-', button_color=('white', '#001480')),
+                sg.Button('Reset', button_color=('white', '#007339'), key='-RESET-'),
+                sg.Exit(button_color=('white', 'firebrick4'), key='Exit')]]
+
+
+tab1_layout =  [[sg.T('Cronometro')],[sg.Text('')],
+          [sg.Text('00:00:00', size=(8, 2), font=('Helvetica', 100),
+                justification='center', key='text')],[sg.Frame('My Frame Title', frame_layout, font='Any 12', title_color='blue', element_justification='c')]]
+
+equipeTab1_layout = [[sg.T('Aqui ficaria a tabela da equipe 1')]]
+equipeTab2_layout = [[sg.T('Aqui ficaria a tabela da equipe 2')]]
+equipeTab3_layout = [[sg.T('Aqui ficaria a tabela da equipe 3')]]
+equipeTab4_layout = [[sg.T('Aqui ficaria a tabela da equipe 4')]]
+
+
+
+tab2_layout = [[sg.T('Tabela com os tempos e Nome da Equipe')],
+               [sg.TabGroup([[sg.Tab('Equipe 1 Table', equipeTab1_layout, element_justification='c'),
+                              sg.Tab('Equipe 2 Table', equipeTab2_layout, element_justification='c'),
+                              sg.Tab('Equipe 3 Table', equipeTab3_layout, element_justification='c'),
+                              sg.Tab('Equipe 4 Table', equipeTab4_layout, element_justification='c')]])]
+               ]
+
+layout = [[sg.TabGroup([[sg.Tab('Cronometro', tab1_layout, element_justification='c'), sg.Tab('Tabela com os tempos e Nome da Equipe', tab2_layout, element_justification='c')]])]]
+window = sg.Window('Running Timer', layout,
+                   resizable=True,
+                   no_titlebar=False,
+                   auto_size_buttons=False,
+                   keep_on_top=True,
+                   grab_anywhere=True,
+                   element_padding=(0, 0),
+                   finalize=True,
+                   element_justification='c',
+                   right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_EXIT)
+run = False
+while True:
+    if run:
+        stopwatch.start()
+    
+# ========================== Conversão e Leitura =================================================================================
+    if True:
+        totalSec = stopwatch.duration
+        millisTotal = 1000*totalSec
+        segundos = int(millisTotal/1000)
+        minutos = int(millisTotal/60000)
+        millis = int(millisTotal)
+        if(segundos >= 59):
+            segundos = segundos % 60
+        if(millis >= 999):
+            millis = millis % 1000
+            
+# ======================================================================================================================
+    
+    
+    
+# =============== Botões Funcionamento =================================================================================  
+    if run:
+        event, values = window.read(timeout=10)
+    else:
+        event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):        # ALWAYS give a way out of program
+        break
+    if event == '-RUN-PAUSE-':
+        if run == False:
+            stopwatch.start()
+            run = True
+        elif run == True:
+            stopwatch.stop()
+            run = False
+    #Tendo apertar 2 vezes o botão reset(só usar caso o carro saia da pista - da para contornar o bug)        
+    if event == '-RESET-':
+        stopwatch.reset()
+        minutos = 0
+        segundos = 0
+        millis = 0
+        run=False
+        print('{:02d}:{:02d}:{:03d}'.format(minutos,segundos,millis))
+# ===========================================================================================================================
+    window['text'].update('{:02d}:{:02d}:{:03d}'.format(minutos,segundos,millis))
+    print(run)
+    window['-RUN-PAUSE-'].update('Run' if not run else 'Pause')
+window.close()
---
+++ Rascunhos/stopwatchMod.py
@@ -0,0 +1,39 @@
+from stopwatch import Stopwatch
+running = False
+minut = 0
+segundos = 0
+millis = 0
+# Argument specifies decimal precision for __str__
+# e.g 2 digits = 1.00, 3 digits = 1.000
+# Optional, defaults to 2
+stopwatch = Stopwatch(2)
+stopwatch.reset()
+# It's just math with time.perf_counter() so there isn't really a task
+# running in background
+
+# stopwatch.stop()  # Stop stopwatch, time freezes
+# stopwatch.start()  # Start it again
+# stopwatch.reset()  # Reset it back to 0
+# stopwatch.restart()  # Reset and start again
+# stopwatch.running  # Wether stopwatch is running
+# x = stopwatch.duration  # Get the duration (in seconds)
+running = True
+while running:
+    if running == True:
+        stopwatch.start()
+    if running:
+        totalSec = stopwatch.duration
+        millisTotal = 1000*totalSec
+        segundos = int(millisTotal/1000)
+        minutos = int(millisTotal/60000)
+        millis = int(millisTotal)
+        if(segundos >= 59):
+            segundos = segundos % 60
+        if(millis >= 999):
+            millis = millis % 1000
+        print("Min: " + str(minutos) + " " + "Segundos: " +
+              str(segundos)+" " + "Milisegundos: " + str(millis))
+    if(minutos == 1):
+        running = False
+        stopwatch.stop
+        stopwatch.reset()
